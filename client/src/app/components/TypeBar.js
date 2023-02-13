5d3eb10585cb4456f934cb54da0aa3d0727691c8
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-13 18:29:45 +0600

    Added home page, list of products, types, brands

---
+++ client/src/app/components/TypeBar.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Context } from "../../index";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const TypeBar = observer(() => {
+    const { device } = useContext(Context);
+
+    return (
+        <ListGroup>
+            {device.types.map((type) => (
+                <ListGroup.Item
+                    active={type.id === device.selectedType.id}
+                    onClick={() => device.setSelectedType(type)}
+                    style={{ cursor: "pointer" }}
+                    key={type.id}
+                >
+                    {type.name}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    );
+});
+
+export default TypeBar;
--- client/src/app/pages/Shop.js
+++ client/src/app/pages/Shop.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import BrandBar from "../components/BrandBar";
+import TypeBar from "../components/TypeBar";
+import DeviceList from "../components/DeviceList";
+import { Context } from "../../index";
 
-const Shop = () => {
-    return <div>Shop</div>;
-};
+const Shop = observer(() => {
+    const { device } = useContext(Context);
+    console.log(device);
+
+    return (
+        <Container>
+            <Row className="mt-2">
+                <Col md={3}>
+                    <TypeBar />
+                </Col>
+                <Col md={9}>
+                    <BrandBar />
+                    <DeviceList />
+                </Col>
+            </Row>
+        </Container>
+    );
+});
 
 export default Shop;
