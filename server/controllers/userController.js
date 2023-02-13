840d57f1e16bd5c04477db22b88dd7e7c86aa6e8
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-13 16:03:08 +0600

    added device acquisition, filtering, pagination

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,17 @@
+const ApiError = require("../error/ApiError");
+
 class UserController {
     async registration(req, res, next) {}
 
     async login(req, res, next) {}
 
-    async check(req, res, next) {}
+    async check(req, res, next) {
+        const { id } = req.query;
+        if (!id) {
+            return next(ApiError.badRequest("Не задан ID"));
+        }
+        res.json(id);
+    }
 }
 
 module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -3,14 +3,22 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const fileUpload = require("express-fileupload");
 const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
+
+// Замыкающий middleware
+app.use(errorHandler);
 
 const start = async () => {
     try {
