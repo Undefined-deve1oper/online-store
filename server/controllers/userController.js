0c8c5f5f18d500938071842eb0cc5eff37dab476
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-13 13:36:42 +0600

    added routing and endpoints

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+class UserController {
+    async registration(req, res, next) {}
+
+    async login(req, res, next) {}
+
+    async check(req, res, next) {}
+}
+
+module.exports = new UserController();
