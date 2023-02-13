baa59fd8f4c5355b8a4db0eaa75eefeff5acd762
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-02-13 18:14:41 +0600

    Added Registration and authorization page

--- client/src/app/pages/Auth.js
+++ client/src/app/pages/Auth.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
+import { Container, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
-const Auth = () => {
-    return <div>Auth</div>;
-};
+const Auth = observer(() => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">
+                    {isLogin ? "Авторизация" : "Регистрация"}
+                </h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш пароль..."
+                        type="password"
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ? (
+                            <div style={{ width: "auto" }}>
+                                Нет аккаунта?{" "}
+                                <NavLink to={REGISTRATION_ROUTE}>
+                                    Зарегистрируйся!
+                                </NavLink>
+                            </div>
+                        ) : (
+                            <div style={{ width: "auto" }}>
+                                Есть аккаунт?{" "}
+                                <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        )}
+                        <Button
+                            style={{ width: "auto" }}
+                            variant={"outline-success"}
+                        >
+                            {isLogin ? "Войти" : "Регистрация"}
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
+    );
+});
 
 export default Auth;
